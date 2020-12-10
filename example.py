433402c3b80c94cf17d570dbea84417333f0a801
sywoo1001 <37800698+sywoo1001@users.noreply.github.com>
2020-12-10 14:41:49 +0900

    Add files via upload
    upgrade

--- example.py
+++ example.py
@@ -1,16 +1,20 @@
 import discord
+from discord.utils import get
+from discord.voice_client import VoiceClient
+from discord import FFmpegPCMAudio
 import asyncio
 import random
 import openpyxl
 import time
+import ffmpeg
 import youtube_dl
-from discord.voice_client import VoiceClient
+from youtube_dl import YoutubeDL
 import crollEx
-import os
-import pytube
+import pafy
 
 client = discord.Client()
 queues = []
+
 
 @client.event
 async def on_ready():
@@ -20,8 +24,10 @@ async def on_ready():
     print("----------------")
     await client.change_presence(activity=discord.Game(name="테스트"))
 
+
 @client.event
 async def on_message(message):
+    global i, voice
     if message.content.startswith("!"):
         author = message.author
         content = message.content
@@ -30,24 +36,15 @@ async def on_message(message):
     def pred(m):
         return m.author == message.author and m.channel == message.channel
 
-    def check_queue():
-        if queues != []:
-            guild = message.guild
-            voice_client = guild.voice_client
-            ab = queues.pop(0)
-            yt = pytube.YouTube(ab)
-            vids = yt.streams.filter(progressive=True).all()
-            parent_dir = os.path.dirname('C://Users//WOO//Documents//python//DiscordBot//audio(video)_data//')
-            voice_client.play(discord.FFmpegPCMAudio(vids[0].download(parent_dir)), after=check_queue())
-
     if message.content == "!인사":
-        await message.channel.send("안녕하세요")
+        author = message.author
+        await message.channel.send(author.mention + " " + "안녕하세요")
 
     if message.content.startswith("!고르기"):
         choice = message.content.split(" ")
-        choicenum = random.randint(1, len(choice)-1)
-        choiceres = choice[choicenum]
-        await message.channel.send(choiceres)
+        choice_num = random.randint(1, len(choice) - 1)
+        choice_res = choice[choice_num]
+        await message.channel.send(choice_res)
 
     if message.content == "!공지":
         file = open("채팅방공지그글.txt")
@@ -60,7 +57,7 @@ async def on_message(message):
         ccl = img[1]
         try:
             ee = int(img[-1])
-            for i in range(2, len(img)-1):
+            for i in range(2, len(img) - 1):
                 ccl = ccl + " " + img[i]
                 i = i + 1
         except:
@@ -72,9 +69,10 @@ async def on_message(message):
 
     if message.content.startswith("!날씨"):
         www = message.content.split(" ")
+        author = message.author
         try:
             wea = crollEx.get_location_ko(www[1])
-            await message.channel.send('현재 ' + www[1] + ' 날씨는 ' + wea + '도 입니다.')
+            await message.channel.send(author.mention + " " + wea)
         except:
             await message.channel.send("알 수 없는 지역입니다.")
 
@@ -126,13 +124,33 @@ async def on_message(message):
         file = openpyxl.load_workbook("디스코드.xlsx")
         sheet = file.active
         memory = message.content.split(" ")
-        for i in range(1,501):
+        for i in range(1, 501):
             if sheet["A" + str(i)].value == str(memory[1]):
                 sheet["A" + str(i)].value = "-"
                 sheet["B" + str(i)].value = " "
                 await message.channel.send("단어를 잊었습니다.")
                 file.save("디스코드.xlsx")
                 break
+
+    if message.content.startswith("!대리할말"):
+        con = message.content.split(" ")
+        tok = int(con[1])
+        author = message.guild.get_member(tok)
+        msg = con[2]
+        await message.channel.send(author.mention + msg)
+
+    if message.content.startswith("!초대"):
+        await message.author.send("https://discordapp.com/oauth2/authorize?client_id=531341952248053775&scope=bot")
+
+    if message.content.startswith("!대화시작"):
+        await message.author.send("반갑습니다.")
+
+    if message.content.startswith("!대리귓말"):
+        con = message.content.split(" ")
+        tok = int(con[1])
+        author = message.guild.get_member(tok)
+        msg = con[2]
+        await author.send(msg)
 
     if message.content == "!참가":
         channel = message.author.voice.channel
@@ -152,40 +170,36 @@ async def on_message(message):
         except:
             await message.channel.send("이미 음성채널에서 연결이 해제되었습니다.")
 
+    if message.content.startswith("!재생"):
+        sentence = message.content.split(" ")
+        url = sentence[1]
+        guild = message.guild
+        FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        video, source = await search(url)
+        voice = discord.utils.get(client.voice_clients, guild=guild)
+        voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+        voice.is_playing()
 
     if message.content == "!일시정지":
-        guild = message.guild
-        voice_client = guild.voice_client
-        id = message.guild.id
-        voice_client.pause()
+        voice.pause()
 
     if message.content == "!정지":
-        guild = message.guild
-        voice_client = guild.voice_client
-        id = message.guild.id
-        voice_client.stop()
+        voice.stop()
 
     if message.content == "!재개":
-        guild = message.guild
-        id = message.guild.id
-        voice_client = guild.voice_client
-        voice_client.resume()
+        voice.resume()
 
     if message.content.startswith("!볼륨"):
         sentence = message.content.split(" ")
         vol = sentence[1]
         guild = message.guild
-        id = message.guild.id
         voice_client = guild.voice_client
         voice_client.source = discord.PCMVolumeTransformer(voice_client.source)
         voice_client.source.volume = vol
 
     if message.content == "!건너뛰기":
-        guild = message.guild
-        voice_client = guild.voice_client
-        id = message.guild.id
-        voice_client.stop()
-        check_queue()
+        voice.stop()
+        await check_queue()
 
     if message.content.startswith("!예약") and not message.content.startswith("!예약목록"):
         sentence = message.content.split(" ")
@@ -195,19 +209,6 @@ async def on_message(message):
 
     if message.content == "!예약목록":
         await message.channel.send("예약 개수는 " + str(len(queues)) + "개 입니다.")
-
-    if message.content.startswith("!재생"):
-        try:
-            sentence = message.content.split(" ")
-            url = sentence[1]
-            guild = message.guild
-            voice_client = guild.voice_client
-            yt = pytube.YouTube(url)
-            vids = yt.streams.filter(progressive=True).all()
-            parent_dir = os.path.dirname('C://Users//WOO//Documents//python//DiscordBot//audio(video)_data//')
-            voice_client.play(discord.FFmpegPCMAudio(vids[0].download(parent_dir)), after=check_queue())
-        except discord.ClientException:
-            await message.channel.send("이미 재생중인 음악이 있습니다. 재생중인 음악 종료 후 다시 시도해주세요.")
 
     if message.content == "!리스트확인":
         queues.append("a")
@@ -220,5 +221,25 @@ async def on_message(message):
     if message.content == "!집사 꺼져":
         await message.channel.send("ㅠㅠ")
         exit(0)
+
+    if message.content.startswith("!@CM"):
+        ch = message.content.split(" ")[1]
+        msg = message.content.split(" ")[2]
+        for i in range(3, len(message.content.split(" "))):
+            msg = msg + " " + message.content.split(" ")[i]
+            i = i + 1
+        await client.get_channel(int(ch)).send(msg)
+
+
+@client.event
+async def check_queue(message):
+    if queues:
+        print("ok")
+
+@client.event
+async def search(query):
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
+        info = ydl.extract_info(query, download=False)
+    return (info, info['formats'][0]['url'])
 
 client.run("NTMxMzQxOTUyMjQ4MDUzNzc1.DxNXaA.E1p7SLcaPE2sDTnbruRwvdnj0b4")
